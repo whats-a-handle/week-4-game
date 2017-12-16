6b7830bf04011fdcb17e2cfcf6bfde78148c5085
whats-a-handle <bromanbroman54@yahoo.com>
2017-12-16 02:02:48 -0800

    Rewrite of most core methods Replaced characterPool array with characterMap Object Created startGame global method to initialize the game by returning a new Game object regeneratePoolElements method will append an element of character object elements to a user specific pool element Added object to store the three pool element values updateCharacterPoolElement takes a characterObject and a string containing the name of the pool to add the character to addCharacters function added which adds an array of characters into an object for refernce in jQuery This allows us to return the name from an avatar image onClick and lookup the related character within the map as the object

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,27 +1,61 @@
-
-//function play(){
-
-	//FOR TESTING ONLY
-
-//}
-
 const Game = function(){
 
-	this.characterPool = [];
 	this.playerCharacter = "NONE";
 	this.round = 1;
+	this.characterMap = {};
+	this.poolElements = {
 
+		neutral : $(".neutral.character-container"),
+		attacker : $(".attacker.character-container"),
+		defender  : $(".defender.character-container")
+
+	} ;
+	
 	
 	this.addCharacter = function(characterObj){
 
-	this.characterPool.push(characterObj);
+		this.characterMap[characterObj.characterName] = characterObj;
+	};
 
+	this.addCharacters = function(characterPool){
+
+		for(let i = 0 ; i < characterPool.length; i++){
+
+			this.characterMap[characterPool[i].characterName] = characterPool[i];
+		}
+	};
+
+	this.updateCharacterPoolElement = function(avatar,pool){
+
+			this.poolElements[pool].append(avatar.avatarElement);		
+	};
+
+	this.regeneratePoolElements = function(avatarList,pool){
+
+		for(let i = 0; i < avatarList.length; i++){
+
+			this.poolElements[pool].append(avatarList[i].avatarElement);
+		}
+
+	}
+
+	//STARTS and also RESETS the game
+	this.resetGame = function(){
+
+	var luke = new Character("luke",100,10,25);
+	var jarjar = new Character("jarjar", 90,5,40);
+	var vader = new Character("vader", 150,20,10);
+	var mace = new Character("mace",200, 40, 30);
+	var characterList = [luke, jarjar,vader,mace];
+	this.addCharacters(characterList);
+	this.regeneratePoolElements(characterList, "neutral");
 
 	};
 
-	this.updateCharacterPoolElement = function(characterObj){
-		$(".neutral.character-container").append(characterObj.avatarElement);
-	};
+	//We want a newGame to be created with this default info
+	//Set the game up upon instantiation
+	this.resetGame();
+	
 	
 
 };
@@ -35,8 +69,10 @@ const Character = function(characterName,baseHealthPoints,baseAttackPower,
 		this.currentHealthPoints = baseHealthPoints;
 
 		this.avatar = "./assets/images/"+ characterName + ".jpg";
-		this.classes = ["character-avatar", "img-fluid"];
-		this.avatarElement = $("<img src=" + this.avatar + " class=" + this.classes.join(" ") + " alt=Responsive image>");
+		this.classes = [ "img-fluid","character-avatar"];
+		//Create an image element with the avatars photo, styling and assign the characterName as the value to be returned
+		this.avatarElement = $("<img src=" + this.avatar + " class=\"" + this.classes.join(" ") + "\" " 
+								+"value=\"" + characterName + "\"" +  "alt=\"Responsive image\">");
 
 		this.baseAttackPower = baseAttackPower;
 		this.currentAttackPower = baseAttackPower;
@@ -106,25 +142,32 @@ const Character = function(characterName,baseHealthPoints,baseAttackPower,
 
 	};
 
+ function startNewGame(){
+
+ 	var newGame = new Game();
+
+ 	return newGame
+ };
+
+
 $( document ).ready(function(){
-console.log("Starting game...");
-	console.log("Creating Game object");
-	var newGame = new Game();
-	console.log("Creating characters");
+	
+	
+	var newGame = startNewGame();
 
-	var char1 = new Character("luke",100,10,25);
-	var char2 = new Character("jarjar",90,5,10);
 
-	newGame.addCharacter(char1);
 
-	newGame.updateCharacterPoolElement(char1);
-	newGame.updateCharacterPoolElement(char2);
-	//newGame.updateCharacterPoolElement(char1);
-	//console.log(newGame);
+	$(".character-avatar.img-fluid").click(function(){
+
+		
+		console.log("You've chosen a character from our character map!");
+		console.log(newGame.characterMap[$(this).attr("value")]);
+
+
+	});
+	
 	
 	
 	
 
 });
-		
-	//FOR TESTING ONLY
